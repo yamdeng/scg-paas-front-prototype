dbae3f5767e2763cd326373d263d338d7310cd53
yamdeng <yamdeng@gmail.com>
2019-01-02 16:37:45 +0900

    연간조회, 미납내역 조회

--- src/components/charge-rate-search/ChargeRateYearSearch.js
+++ src/components/charge-rate-search/ChargeRateYearSearch.js
@@ -11,12 +11,13 @@ import {
   Bar,
   ComposedChart
 } from 'recharts';
+import { Table } from 'reactstrap';
 
 class ChargeRateYearSearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      allMonthChartData: []
     };
   }
 
@@ -34,25 +35,53 @@ class ChargeRateYearSearch extends React.Component {
   render() {
     return (
       <div>
-        <ComposedChart
-          data={this.state.recentUseChartData}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <XAxis dataKey="month" />
-          <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
-          <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="useGas"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-            yAxisId="left"
-          />
-          <Bar dataKey="useAmt" yAxisId="right" barSize={20} fill="#413ea0" />
-        </ComposedChart>
+        <div style={{ overflow: 'scroll' }}>
+          <ComposedChart
+            data={this.state.allMonthChartData}
+            width={1500}
+            height={300}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="date" />
+            <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+            <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="chargeAmt"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+              yAxisId="left"
+            />
+            <Bar dataKey="dueAmt" yAxisId="right" barSize={20} fill="#413ea0" />
+          </ComposedChart>
+        </div>
+        <div>
+          <Table>
+            <thead>
+              <tr>
+                <th>청구년월</th>
+                <th>사용량(m)</th>
+                <th>청구금액</th>
+                <th>미납금액</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.allMonthChartData.map(info => {
+                return (
+                  <tr>
+                    <td>{info.date}</td>
+                    <td>{info.useGas}</td>
+                    <td>{info.chargeAmt}</td>
+                    <td>{info.dueAmt}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
       </div>
     );
   }
--- src/components/charge-rate-search/PayOffHistory.js
+++ src/components/charge-rate-search/PayOffHistory.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Table } from 'reactstrap';
+import Api from '../../utils/Api';
+import Config from '../../config/Config';
 
 class PayOffHistory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      payOffHistoryInfo: [],
+      totalPayOffUseGas: 0,
+      totalPayOffChargeAmt: 0,
+      totalPayOffDueAmt: 0
+    };
+  }
+
+  componentDidMount() {
+    Api.get('payOffHistory/' + Config.contractNo).then(result => {
+      this.setState({
+        payOffHistoryInfo: result.data.payOffHistoryInfo,
+        totalPayOffUseGas: result.data.totalPayOffUseGas,
+        totalPayOffChargeAmt: result.data.totalPayOffChargeAmt,
+        totalPayOffDueAmt: result.data.totalPayOffDueAmt
+      });
+    });
+  }
   render() {
-    return <div>dd</div>;
+    return (
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>청구년월</th>
+              <th>사용량(m)</th>
+              <th>청구금액</th>
+              <th>미납금액</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.payOffHistoryInfo.map(info => {
+              return (
+                <tr>
+                  <td>{info.date}</td>
+                  <td>{info.useGas}</td>
+                  <td>{info.chargeAmt.toLocaleString() + '원'}</td>
+                  <td>{info.dueAmt.toLocaleString() + '원'}</td>
+                </tr>
+              );
+            })}
+            <tr style={{ fontWeight: 'bold' }}>
+              <td>합계</td>
+              <td>{this.state.totalPayOffUseGas}</td>
+              <td>{this.state.totalPayOffChargeAmt.toLocaleString() + '원'}</td>
+              <td>{this.state.totalPayOffDueAmt.toLocaleString() + '원'}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
