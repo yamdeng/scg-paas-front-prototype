2b73db5515d6e9525b9b5a10b1563268f18d00c0
yamdeng <yamdeng@gmail.com>
2019-01-04 14:27:39 +0900

    프로필, 설정 정보 반영

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Api from '../utils/Api';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import Config from '../config/Config';
+
+const styles = {
+  avatar: {
+    margin: 10,
+    width: 70,
+    height: 70
+  }
+};
 
 @withRouter
 @inject('appStore')
@@ -24,13 +36,24 @@ class Profile extends React.Component {
   }
 
   render() {
+    let classes = this.props.classes;
     return (
-      <ListGroup>
-        <ListGroupItem>로그이ID : {this.state.data.loginId}</ListGroupItem>
-        <ListGroupItem>이름 : {this.state.data.name}</ListGroupItem>
-      </ListGroup>
+      <React.Fragment>
+        <Grid container justify="center" alignItems="center">
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+            className={classes.avatar}
+          />
+        </Grid>
+        <ListGroup>
+          <ListGroupItem>로그인ID : {this.state.data.loginId}</ListGroupItem>
+          <ListGroupItem>이름 : {this.state.data.name}</ListGroupItem>
+          <ListGroupItem>계약관리번호 : {Config.contractNo}</ListGroupItem>
+        </ListGroup>
+      </React.Fragment>
     );
   }
 }
 
-export default Profile;
+export default withStyles(styles)(Profile);
--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,16 +1,72 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
 
 @withRouter
 @inject('appStore')
 @observer
 class Setting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPush: true,
+      isEvent: false,
+      isSync: true
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(name, event) {
+    this.setState({ [name]: event.target.checked });
+  }
+
   componentDidMount() {
     this.props.appStore.changeHeadTitle('설정');
   }
+
   render() {
-    return <div>설정</div>;
+    return (
+      <FormControl component="fieldset" style={{ marginLeft: 10 }}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={this.state.isPush}
+                onChange={event => this.handleChange('isPush', event)}
+                value="isPush"
+              />
+            }
+            label="기본 Push 알림 수신"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={this.state.isEvent}
+                onChange={event => this.handleChange('isEvent', event)}
+                value="isEvent"
+              />
+            }
+            label="혜택 알림 수신"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={this.state.isSync}
+                onChange={event => this.handleChange('isSync', event)}
+                value="isSync"
+              />
+            }
+            label="자동 동기화 여부"
+          />
+        </FormGroup>
+      </FormControl>
+    );
   }
 }
 
