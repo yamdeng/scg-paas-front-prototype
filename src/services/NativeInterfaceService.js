3b751f9dc0e41035a64ca269ff753cf47b7663c6
yamdeng <yamdeng@gmail.com>
2019-01-04 16:20:13 +0900

    디바이스 연동 인터페이스 와꾸 fix

--- src/services/NativeInterfaceService.js
+++ src/services/NativeInterfaceService.js
@@ -1,9 +1,32 @@
+import DeviceUtil from '../utils/DeviceUtil';
+import stores from '../stores/stores';
+
 class NativeInterfaceService {
-  disableBackButton() {}
-  getDeviceInfo() {}
-  setAppVersion() {}
-  openCamera() {}
-  getGps() {}
+  disableBackButton() {
+    if (DeviceUtil.isAndroid) {
+      window.android.disableBackButton();
+    }
+  }
+  getDeviceInfo() {
+    if (DeviceUtil.isAndroid) {
+      window.android.getDeviceInfo();
+    }
+  }
+  setAppVersion() {
+    if (DeviceUtil.isAndroid) {
+      window.android.setAppVersion(stores.appStore.appVersion);
+    }
+  }
+  openCamera() {
+    if (DeviceUtil.isAndroid) {
+      window.android.openCamera();
+    }
+  }
+  getGps() {
+    if (DeviceUtil.isAndroid) {
+      window.android.getGps();
+    }
+  }
 }
 
 export default new NativeInterfaceService();
