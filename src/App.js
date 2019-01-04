3ebc18ca97c1fafb7c72d384db68a184d35eb99e
yamdeng <yamdeng@gmail.com>
2019-01-04 10:32:42 +0900

    DrawerNavigation 반영

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import { Route, withRouter } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import DrawerNavigation from './components/DrawerNavigation';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Setting from './components/Setting';
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navigation />
+        <DrawerNavigation />
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/profile" component={Profile} />
