542e87b1d72d3d77e8b9343586cfcc32ee31083f
yamdeng <yamdeng@gmail.com>
2019-01-04 18:20:11 +0900

    네이티브 인터페이스 디바이스 호출 부분도 추가

--- src/services/NativeEventService.js
+++ src/services/NativeEventService.js
@@ -1,10 +1,21 @@
-import DeviceUtil from '../utils/DeviceUtil';
+// eslint-disable-next-line
 import stores from '../stores/stores';
 
 class NativeEventService {
   initEventListener() {
-    $(window).on('getDeviceInfo', function() {
-      console.log('test33333 : ' + stores.appStore.headTitle);
+    $(window).on('getDeviceInfo', function(event, data) {
+      // eslint-disable-next-line
+      console.log('getDeviceInfo');
+    });
+
+    $(window).on('sendImage', function(event, data) {
+      // eslint-disable-next-line
+      console.log('sendImage');
+    });
+
+    $(window).on('getGps', function(event, data) {
+      // eslint-disable-next-line
+      console.log('getGps : ' + data);
     });
   }
 }
