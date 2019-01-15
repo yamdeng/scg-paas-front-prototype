8d2cf9f19557b1bf3909adae221c4173bbb5e514
yamdeng <yamdeng@gmail.com>
2019-01-15 10:07:40 +0900

    tel, alert, confirm 테스트

--- src/components/front-issue/DeviceBasic.js
+++ src/components/front-issue/DeviceBasic.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 @withRouter
 @inject('appStore')
@@ -9,6 +10,18 @@ class DeviceBasic extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleAlert = this.handleAlert.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+  }
+
+  handleAlert() {
+    alert('alert call?');
+  }
+
+  handleConfirm() {
+    if (confirm('confirm call?')) {
+      alert('confirm success');
+    }
   }
 
   componentDidMount() {
@@ -39,6 +52,16 @@ class DeviceBasic extends React.Component {
           <br />
           <input type="time" />
         </form>
+        <Button color="primary" size="lg" block onClick={this.handleAlert}>
+          alert
+        </Button>
+        <Button color="primary" size="lg" block onClick={this.handleConfirm}>
+          confirm
+        </Button>
+        <br />
+        <p>
+          전화 테스트 : <a href="tel:010-7338-4183">1-847-555-5555</a>
+        </p>
       </div>
     );
   }
